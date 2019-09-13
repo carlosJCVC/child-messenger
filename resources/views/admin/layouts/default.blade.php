e9acb506d904fa4a5fe425cf589421c57a6cc16b
carlos <carlosveizaga.jcvc@gmail.com>
2019-09-13 11:14:13 -0400

    layout with grid added

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -11,12 +11,85 @@
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 </head>
 <body>
-    @section('sidebar')
-        This is the master sidebar.
-    @show
 
-    <div class="container">
-        @yield('content')
+    <div class="wrapper">
+        <header class="header">
+            <a href="#" class="logo">
+                <img src="{{ asset('assets/img/logo.png') }}" alt="logo">
+            </a>
+    
+            <nav>
+                <div>
+                    <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
+                        <i class="fas fa-bars fa-2x"></i>
+                    </a>
+                </div>
+        
+                <div class="navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown user user-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <img src="{!! asset('assets/img/authors/avatar3.jpg') !!} " width="35" class="img-circle img-responsive pull-left" height="35" alt="riot">
+                                <div class="riot">
+                                    <div>
+                                        <p class="user_name_max">Carlos Veizaga</p>
+                                        <span>
+                                            <i class="caret"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <!-- User image -->
+                                <li class="user-header bg-light-blue">
+                                    <img src="{!! asset('assets/img/authors/avatar3.jpg') !!}" class="img-responsive img-circle" alt="User Image">
+                                    <p class="topprofiletext">Carlos Veizaga</p>
+                                </li>
+                                <!-- Menu Body -->
+                                <li>
+                                    <a href="#">
+                                        <i class="livicon" data-name="user" data-s="18"></i>
+                                        My Profile
+                                    </a>
+                                </li>
+                                <li role="presentation"></li>
+                                <li>
+                                    <a href="#">
+                                        <i class="livicon" data-name="gears" data-s="18"></i>
+                                        Account Settings
+                                    </a>
+                                </li>
+                                <!-- Menu Footer-->
+                                <li class="user-footer">
+                                    <div class="pull-left">
+                                        <a href="#">
+                                            <i class="livicon" data-name="lock" data-size="16" data-c="#555555" data-hc="#555555" data-loop="true"></i>
+                                            Lock
+                                        </a>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a href="#">
+                                            <i class="livicon" data-name="sign-out" data-s="18"></i>
+                                            Logout
+                                        </a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        
+        <aside class="aside">
+            sidebar
+        </aside>
+
+        <main class="main">
+            <div class="container">
+                @yield('content')
+            </div>
+        </main>
     </div>
 
     @yield('scripts')
